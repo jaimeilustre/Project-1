2cdcae23ca22701e86f51b3770fea559b7c6c019
Jaime Ilustre <jaimeilustre@yahoo.com>
2023-11-01 12:42:52 +0100

    Obstacles added but still problem with spawning and collision detection

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ document.addEventListener("keydown", (e) => {
 const player = new Player();
 
 
-// Creating new opponent
+// Creating opponent
 class Opponent {
     constructor() {
         // Setting default starting position
@@ -82,31 +82,83 @@ class Opponent {
 
         // Setting opponent dimensions
         this.height = 150;
-        this.width = 260;
+        this.width = 300;
     
         // Dom manipulation
-        this.opponentElm1 = document.getElementById("opponent1");
-        this.opponentElm1.style.width = this.width + "px";
-        this.opponentElm1.style.height = this.height + "px";
-        this.opponentElm1.style.left= this.positionX + "vw";
-        this.opponentElm1.style.bottom = this.positionY + "vh";    
+        this.opponentElm = document.getElementById("opponent1");
+        this.opponentElm.style.width = this.width + "px";
+        this.opponentElm.style.height = this.height + "px";
+        this.opponentElm.style.left= this.positionX + "vw";
+        this.opponentElm.style.bottom = this.positionY + "vh";    
     }
 }
 
-// Invoking new opponent
-const opponent1 = new Opponent()
+// Invoking opponent
+const opponent = new Opponent()
 
-/*
-if (
-    player.positionX < opponent1.positionX + opponent1.width &&
-    player.positionX + player.width > opponent1.positionX &&
-    player.positionY < opponent1.positionY + opponent1.height &&
-    player.positionY + player.height > opponent1.positionY
-) {
-    // Collision detected!
-    location.href = "./gameover.html"
+// Creating new class for obstacles
+
+class Obstacle {
+    constructor() {
+        this.width = 260;
+        this.height = 150;
+        this.positionX = 100;
+        this.positionY = 100;
+        this.obstacleElm = null;
+
+        this.createDomElement()
+    }
+    createDomElement() {
+        this.obstacleElm = document.createElement('div');
+
+        this.obstacleElm.classList.add('obstacle');
+        this.obstacleElm.style.width = this.width + 'px';
+        this.obstacleElm.style.height = this.height + 'px';
+        this.obstacleElm.style.left = this.positionX + 'vw';
+        this.obstacleElm.style.bottom = this.positionY + 'vh'
+
+        const parentElm = document.getElementById('track');
+        parentElm.appendChild(this.obstacleElm)
+    }
+    moveLeft() {
+        this.positionX--;
+        this.obstacleElm.style.left = this.positionX + 'vw'; 
+    }
 }
-*/
+
+// Creating obstacles
+
+const obstaclesArr = []
+
+setInterval(() => {
+    const newObstacle = new Obstacle ();
+    obstaclesArr.push(newObstacle)
+}, 1000)
+
+
+setInterval(() => {
+    obstaclesArr.forEach((obstacleInstance) => {
+        obstacleInstance.moveLeft();
+        if(obstacleInstance.positionY < 0 - obstacleInstance.width) {
+            obstacleInstance.obstacleElm.remove()
+        }
+        if (
+            player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+            player.positionX + player.width > obstacleInstance.positionX &&
+            player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+            player.positionY + player.height > obstacleInstance.positionY
+          ) {
+            // Collision detected!
+            //location.href = "./gameover.html"
+          }
+    })
+}, 50)
+
+
+
+
+
+
 
 
 
